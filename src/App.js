c7c2d94b883317d9851d01b363c34642d3e291ff
Xmushan <1136053514@qq.com>
2020-07-23 21:44:31 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+
+
+const App = () =>{
+  const [data,setData] = useState([])
+  const handleClick = () =>{
+    fetch('https://api.apiopen.top/getJoke?page=1&count=2&type=video').then(res => {
+      return res.json()
+    }).then(msg =>{
+      console.log(msg)
+      setData(msg.result)
+      console.log(data)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={handleClick}>
+      34
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
